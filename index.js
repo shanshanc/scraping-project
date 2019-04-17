6cb194c3eae7c5b9cd8d9207b98c8afe628100fa
Shanshan Chen <shanshanc.chen@gmail.com>
2019-04-17 18:37:08 +0200

    get product links on the homepage

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const vo = require('vo');
 const urlHomepage = 'https://enhancedecommerce.appspot.com/';
 const urlProduct1 = 'https://enhancedecommerce.appspot.com/item/9bdd2';
 
+const run = function * () {
+  let result = yield nightmare
+    .goto(urlHomepage)
+    .inject('js', 'jquery.min.js')
+    .wait(1000)
+    .evaluate(() => {
+      // return document.querySelector(".thumbnail a").href;
+      let elements = document.getElementsByClassName("thumbnail");
+      let links = [];
+      for (let i = 0; i < elements.length; i++) {
+        let elem = elements[i].querySelector("a").href;
+        // console.log(elem);
+        links.push(elem);
+      }
+      return links;
+      // let hrefs = [];
+      // let links = document.querySelectorAll(".thumbnail > a").href;
+      // return links;
+    });
+  
+  yield nightmare.end();
+  return result;
+
+};
+
+vo(run)((err, result) => {
+  console.log(result);
+});
+
+
 // Get a list of products from the all t-shirts page
 // let selector = 'thumbnail';
 // nightmare
@@ -27,16 +57,16 @@ const urlProduct1 = 'https://enhancedecommerce.appspot.com/item/9bdd2';
 
 
 // Click to a product page from the homepage
-nightmare
-  .goto(urlHomepage)
-  .click('#homepage-9bdd2-1')
-  .wait(2000)
-  .evaluate(() => document.querySelector(".productCard").innerText)
-  .end()
-  .then(console.log)
-  .catch(err => {
-    console.log('failed: ', err);
-  });
+// nightmare
+//   .goto(urlHomepage)
+//   .click('#homepage-9bdd2-1')
+//   .wait(2000)
+//   .evaluate(() => document.querySelector(".productCard").innerText)
+//   .end()
+//   .then(console.log)
+//   .catch(err => {
+//     console.log('failed: ', err);
+//   });
 
 // Get product id
 // let productId = {};
