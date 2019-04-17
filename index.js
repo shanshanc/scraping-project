e18801353132f62908e9638703f50d8fed1bc6ac
Shanshan Chen <shanshanc.chen@gmail.com>
2019-04-17 17:35:56 +0200

    pick a product, drill-down, and get data

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true }); // Pass { show: true } to the nightmare constructor to have it create a visible, rendered window where you can watch what is happening.
+const vo = require('vo');
 
 const urlHomepage = 'https://enhancedecommerce.appspot.com/';
 const urlProduct1 = 'https://enhancedecommerce.appspot.com/item/9bdd2';
 
 // Get a list of products from the all t-shirts page
-nightmare
-  .goto(urlHomepage)
-  .wait(1000)
-  .evaluate(() => {
-    console.log('elements');
-    let elements = document.querySelectorAll('.thumbnail > a');
-    return elements;
-  })
-  .end()
-  .then()
-  .catch(err => {
-    console.log('failed: ', err);
-  });
-
-
-// Click to a product page from the homepage
+// let selector = 'thumbnail';
 // nightmare
 //   .goto(urlHomepage)
-//   .click('#homepage-9bdd2-1')
-//   .wait(3000)
+//   .wait(1000)
+//   .evaluate((selector) => {
+//     let elements =  document.getElementsByClassName(selector);
+//     for (let i = 0; i < elements.length; i++) {
+//       console.log(elements[i]);
+//     }
+//     // return elements;
+//   }, selector)
 //   .end()
-//   .then(console.log)
+//   .then(data => {
+//     console.log(data);
+//   })
 //   .catch(err => {
 //     console.log('failed: ', err);
 //   });
+
+
+// Click to a product page from the homepage
+nightmare
+  .goto(urlHomepage)
+  .click('#homepage-9bdd2-1')
+  .wait(2000)
+  .evaluate(() => document.querySelector(".productCard").innerText)
+  .end()
+  .then(console.log)
+  .catch(err => {
+    console.log('failed: ', err);
+  });
 
 // Get product id
 // let productId = {};
