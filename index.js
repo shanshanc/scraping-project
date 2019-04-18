3e74f58d4cc9fb627c68b6692f1864fccc28c94d
Shanshan Chen <shanshanc.chen@gmail.com>
2019-04-18 09:53:23 +0200

    save product data in an object

--- index.js
+++ index.js
@@ -1,59 +1,81 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true }); // Pass { show: true } to the nightmare constructor to have it create a visible, rendered window where you can watch what is happening.
 const vo = require('vo');
+const async = require('async');
 
 const urlHomepage = 'https://enhancedecommerce.appspot.com/';
 const urlProduct1 = 'https://enhancedecommerce.appspot.com/item/9bdd2';
+let urlProducts = [];
+// let obj = {};
 
-const run = function * () {
+// Get product links from the homepage
+const getLinks = function * () {
   let result = yield nightmare
     .goto(urlHomepage)
     .inject('js', 'jquery.min.js')
     .wait(1000)
     .evaluate(() => {
-      // return document.querySelector(".thumbnail a").href;
       let elements = document.getElementsByClassName("thumbnail");
       let links = [];
       for (let i = 0; i < elements.length; i++) {
+        // get product url
         let elem = elements[i].querySelector("a").href;
-        // console.log(elem);
-        links.push(elem);
+        links.push(elem); 
       }
       return links;
-      // let hrefs = [];
-      // let links = document.querySelectorAll(".thumbnail > a").href;
-      // return links;
     });
   
   yield nightmare.end();
+  
   return result;
-
 };
 
-vo(run)((err, result) => {
-  console.log(result);
+let proudctObj = {};
+
+vo(getLinks)((err, result) => {
+  urlProducts = result.slice();
+
+  // Visit product page and get product data
+  function getProductData(url, cb) {
+    const nightmare = new Nightmare();
+    // get product id from url
+    const beginStr = url.indexOf('item/') + 5;
+    const endStr = url.length;
+    const id = url.slice(beginStr, endStr);
+
+    nightmare
+      .goto(url)
+      .wait(1000)
+      .evaluate(() => {
+        return document.querySelector(".productCard").innerText;
+       })
+      .end()
+      .then(data => {
+        let splitted = data.split('\n');
+        let name = splitted[0];
+        let price = parseFloat(splitted[1].slice(-5));
+        let description = splitted[2];
+        let product = {id: id, url: url, name: name, price: price, description: description};
+        // console.log('product: ', product);
+        proudctObj[id] = product;
+        // console.log('name: ', name, 'price: ', price, 'desc: ', description);
+        console.log(proudctObj);
+      })
+      .catch(err => {
+        console.log('failed: ', err);
+    });
+
+    
+  }
+
+  async.each(urlProducts, getProductData, function (err) {
+    if (err) console.log('Error: ', err);
+    console.log('done');
+    // console.log(proudctObj);
+  });
+
 });
 
-
-// Get a list of products from the all t-shirts page
-// let selector = 'thumbnail';
-// nightmare
-//   .goto(urlHomepage)
-//   .wait(1000)
-//   .evaluate((selector) => {
-//     let elements =  document.getElementsByClassName(selector);
-//     for (let i = 0; i < elements.length; i++) {
-//       console.log(elements[i]);
-//     }
-//     // return elements;
-//   }, selector)
-//   .end()
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.log('failed: ', err);
-//   });
 
 
 // Click to a product page from the homepage
